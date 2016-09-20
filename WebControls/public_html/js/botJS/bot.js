efba60c34d436abb7ebbf1885b447238f0d38aa6
Alex <asbattarbee@outlook.com.au>
2016-09-20 15:36:55 +0930

    Started addition of new blocks with category in toolbox and a start of generator output.

---
+++ WebControls/public_html/js/botJS/bot.js
@@ -0,0 +1,19 @@
+/**
+ * Custom Blocks for Blockly, for the FH_Tbot Project
+ */
+
+
+
+Blockly.JavaScript['bot_turn_left'] = function(block) {
+  var angle_left_turn = block.getFieldValue('Left Turn');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['bot_turn_right'] = function(block) {
+  var angle_left_right = block.getFieldValue('Right Turn');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
